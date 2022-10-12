67fe933a60bcc36fd0f24bc62aa7bdeb911b1d9f
kenworw <kenworw@users.noreply.github.com>
2022-10-12 23:00:06 +0300

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
 """
-python script that lists all states from the database hbtn_0e_0_usa with
-a given name
+Displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
 """
 
-import MySQLdb
-from sys import argv
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb as mysql
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                        passwd=argv[2], db=argv[3], charset="utf8")
+    try:
+        db = mysql.connect(host='localhost', port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3])
+    except Exception:
+        print('Failed to connect to the database')
+        exit(0)
+
+    searched = argv[4]
+
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
-    id ASC".format(argv[4]))
-    rows = cursor.fetchall()
-    for row in rows:
-        if row[1] == argv[4]:
-            print(row)
+
+    cursor.execute("SELECT * FROM states WHERE name = BINARY '{:s}' \
+                    ORDER BY id ASC;".format(searched))
+
+    result_query = cursor.fetchall()
+
+    for row in result_query:
+        print(row)
+
     cursor.close()
     db.close()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
 """
-Lists all states with a name starting with
-N (upper N) from the database hbtn_0e_0_usa
+Displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
+(Safe from SQL Injection)
 """
 
 if __name__ == '__main__':
     from sys import argv
     import MySQLdb as mysql
+    import re
+
+    if (len(argv) != 5):
+        print('Use: username, password, database name, state name')
+        exit(1)
+
+    searched = ' '.join(argv[4].split())
+
+    if (re.search('^[a-zA-Z ]+$', searched) is None):
+        print('Enter a valid name state (example: Arizona)')
+        exit(1)
 
     try:
         db = mysql.connect(host='localhost', port=3306, user=argv[1],
@@ -18,7 +30,7 @@ if __name__ == '__main__':
     cursor = db.cursor()
 
     cursor.execute("SELECT * FROM states \
-                    WHERE name LIKE BINARY 'N%' ORDER BY id ASC;")
+                    WHERE name = '{:s}' ORDER BY id ASC;".format(searched))
 
     result_query = cursor.fetchall()
 
